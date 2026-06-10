3a57e207dcd15bd8a0903065dac1dd1ee251fec7
unknown <roman_23_07_90@mail.ru>
2026-06-10 21:08:17 +0500

    add func for clear names

---
+++ src/main.py
@@ -0,0 +1,26 @@
+
+
+def clear_names(file_name: str) -> list:
+    """Функция для очистки имён от лишних символов"""
+    new_names_list = list()
+    with open('data/' + file_name, 'r', encoding='utf-8-sig') as names_file:
+        names_list = names_file.read().split()
+
+    for name_item in names_list:
+        new_name = ''
+        for symbol in name_item:
+            if symbol.isalpha():
+                new_name += symbol
+        if new_name.isalpha():
+            new_names_list.append(new_name)
+    return new_names_list
+
+
+if __name__ == '__main__':
+    cleared_name = clear_names("names.txt")
+
+    for i in cleared_name:
+        print(i)
+
+
+
