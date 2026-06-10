f8e6e1447fc9923f4d983ba7a15030425aff9b03
unknown <roman_23_07_90@mail.ru>
2026-06-10 21:52:16 +0500

    filter for russian names

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-
+import re
 
 def clear_names(file_name: str) -> list:
     """Функция для очистки имён от лишних символов"""
@@ -16,11 +16,23 @@ def clear_names(file_name: str) -> list:
     return new_names_list
 
 
+def is_cyrillic(name_item: str) -> bool:
+    """Проверка на вхождение кирилыцы в строку"""
+    return bool(re.search(pattern='[а-яА-Я]', string=name_item))
+
+
+def filter_russian_names(names_list: list) -> list:
+    """Фильтрация имён написанных на русском"""
+
+
+    new_names_list = list()
+    for name_item in names_list:
+        if is_cyrillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
+
 if __name__ == '__main__':
     cleared_name = clear_names("names.txt")
 
-    for i in cleared_name:
-        print(i)
-
-
-
+    print(filter_russian_names(cleared_name))
