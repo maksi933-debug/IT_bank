78bc4a624d4d060ff51c4597937850a5d5797fa1
unknown <roman_23_07_90@mail.ru>
2026-06-11 20:06:14 +0500

    filter english names

--- src/main.py
+++ src/main.py
@@ -31,6 +31,16 @@ def filter_russian_names(names_list: list) -> list:
             new_names_list.append(name_item)
     return new_names_list
 
+def filter_english_names(names_list: list) -> list:
+    """Фильтрация имён написанных на английском"""
+
+
+    new_names_list = list()
+    for name_item in names_list:
+        if not is_cyrillic(name_item):
+            new_names_list.append(name_item)
+    return new_names_list
+
 
 def save_to_file(file_name: str, data: str)  -> None:
     """Сохраняет данные в файл"""
@@ -43,5 +53,15 @@ if __name__ == '__main__':
     filtered_names = filter_russian_names(cleared_name)
     save_to_file(
       'russian_names.txt',
+      '\n'.join(filtered_names)
+    )
+
+
+if __name__ == '__main__':
+    cleared_name = clear_names("names.txt")
+
+    filtered_names = filter_english_names(cleared_name)
+    save_to_file(
+      'english_names.txt',
       '\n'.join(filtered_names)
     )
