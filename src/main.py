231ec6c4d3ee2c90adbb764ba6029fa4bcdf63dd
unknown <roman_23_07_90@mail.ru>
2026-06-11 20:00:59 +0500

    save data in file

--- src/main.py
+++ src/main.py
@@ -32,7 +32,16 @@ def filter_russian_names(names_list: list) -> list:
     return new_names_list
 
 
+def save_to_file(file_name: str, data: str)  -> None:
+    """Сохраняет данные в файл"""
+    with open('data/' + file_name, 'w') as names_file:
+        names_file.write(data)
+
 if __name__ == '__main__':
     cleared_name = clear_names("names.txt")
 
-    print(filter_russian_names(cleared_name))
+    filtered_names = filter_russian_names(cleared_name)
+    save_to_file(
+      'russian_names.txt',
+      '\n'.join(filtered_names)
+    )
